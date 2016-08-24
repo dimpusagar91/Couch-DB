89441ebf9925bec84c5f473845848385db576f29
dimpusagar91 <dimpusagar91@gmail.com>
2016-08-24 11:24:09 +0530

    Update couch.py

--- couch.py
+++ couch.py
@@ -1,14 +1,106 @@
-import couchdb
-from couchdb import *
+#! /usr/bin/python2.6
 
-couch = couchdb.Server('http://127.0.0.1:5984/')
-db = couch.create('testing123')
-doc = {'foo': 'bar', 'age ': ' 20 ', ' name ' : ' xyz ' }
-db.save(doc)
-print "document added : ",doc
+import httplib, json
 
-#Deleting the doc
-db.delete(doc)
+def prettyPrint(s):
+    """Prettyprints the json response of an HTTPResponse object"""
 
-#Deleting the database
-couch.delete('testing123')
+    # HTTPResponse instance -> Python object -> str
+    print simplejson.dumps(json.loads(s.read()), sort_keys=True, indent=4)
+
+class Couch:
+    """Basic wrapper class for operations on a couchDB"""
+
+    def __init__(self, host, port=5984, options=None):
+        self.host = host
+        self.port = port
+
+    def connect(self):
+        return httplib.HTTPConnection(self.host, self.port) # No close()
+
+    # Database operations
+
+    def createDb(self, dbName):
+        """Creates a new database on the server"""
+
+        r = self.put(''.join(['/',dbName,'/']), "")
+        return(r)
+
+    def deleteDb(self, dbName):
+        """Deletes the database on the server"""
+
+        r = self.delete(''.join(['/',dbName,'/']))
+        return(r)
+
+    def listDb(self):
+        """List the databases on the server"""
+
+        return(self.get('/_all_dbs'))
+
+    def infoDb(self, dbName):
+        """Returns info about the couchDB"""
+        r = self.get(''.join(['/', dbName, '/']))
+        return(r)
+
+    # Document operations
+
+    def listDoc(self, dbName):
+        """List all documents in a given database"""
+
+        r = self.get(''.join(['/', dbName, '/', '_all_docs']))
+        return(r)
+
+    def openDoc(self, dbName, docId):
+        """Open a document in a given database"""
+        r = self.get(''.join(['/', dbName, '/', docId,]))
+        return(r)
+
+    def saveDoc(self, dbName, body, docId=None):
+        """Save/create a document to/in a given database"""
+        if docId:
+            r = self.put(''.join(['/', dbName, '/', docId]), body)
+        else:
+            r = self.post(''.join(['/', dbName, '/']), body)
+        return(r)
+
+    def deleteDoc(self, dbName, docId):
+        """Delets the document"""
+        p = openDoc(self, dbName, docId)
+        rev_id = p.read()[_rev]
+        r = self.delete(''.join(['/', dbName, '/', docId, '?rev=', rev_id]))
+        return(r)
+        
+    def update(self, dbName, doc, docId):
+        """Delets the document"""
+        p = openDoc(self, dbName, docId)
+        rev_id = p.read()[_rev]
+        r = self.put(''.join(['/', dbName, '/', docId, '?rev=', rev_id]), body)
+        return(r)
+
+    # Basic http methods
+
+    def get(self, uri):
+        c = self.connect()
+        headers = {"Accept": "application/json"}
+        c.request("GET", uri, None, headers)
+        return c.getresponse()
+
+    def post(self, uri, body):
+        c = self.connect()
+        headers = {"Content-type": "application/json"}
+        c.request('POST', uri, body, headers)
+        return c.getresponse()
+
+    def put(self, uri, body):
+        c = self.connect()
+        if len(body) > 0:
+            headers = {"Content-type": "application/json"}
+            c.request("PUT", uri, body, headers)
+        else:
+            c.request("PUT", uri, body)
+        return c.getresponse()
+
+    def delete(self, uri):
+        c = self.connect()
+        c.request("DELETE", uri)
+        return c.getresponse()
